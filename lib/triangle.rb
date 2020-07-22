6221944a1ec34b65080f06012988f93318aebf00
Marcus Moreno <legendary.1198@gmail.com>
2020-07-22 21:49:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -39,7 +39,7 @@ def validate_triangle
   
   [a, b, c].each do |side|
     
-    real_triangle << false of side <= 0 
+    real_triangle << false if side <= 0 
     
     raise TriangleError if real_triangle.include?(false)
   end
